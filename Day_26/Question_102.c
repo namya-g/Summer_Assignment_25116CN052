ccf2acaefff2ec9214a94fa8268789fcc684faad
namyagupta15-design <namyagupta15@gmail.com>
2026-06-26 20:53:46 +0530

    git init git add Day_26 git commit -m “Day_26 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

---
+++ Day_26/Question_102.c
@@ -0,0 +1,31 @@
+//Q102 Write a program to Create voting eligibility system.
+#include<stdio.h>
+int main()
+{
+    char ch=' ';
+    while((ch!='N')&&(ch!='n'))
+    {
+        int age,citizen;
+        printf("Enter age of the person:- ");
+        scanf("%d",&age);
+        printf("Does the person have a registered Voter ID (Enter 1 for Yes. Enter 0 for No.):- ");
+        scanf("%d",&citizen);
+        if(age>0)
+        {
+            if((age>=18)&&(citizen==1))
+            {
+                printf("The person is eligible to vote.\n");
+            }
+            else
+            {
+                printf("The person is not eligible to vote.\n");
+            }
+        }
+        else
+        {
+            printf("Enter correct age of the person.\n");
+        }
+        printf("Do you want to check again (y/n):- ");
+        scanf(" %c",&ch);
+    }
+}
