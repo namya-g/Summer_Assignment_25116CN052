63a232f2c06e571162f6995c60df0acc38a7f4ae
namyagupta15-design <namyagupta15@gmail.com>
2026-06-26 22:20:58 +0530

    git init git add Day_26 git commit -m “Day_26 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_26/Question_103.c
+++ Day_26/Question_103.c
@@ -1,16 +1,30 @@
 //Q103 Write a program to Create ATM simulation.
 #include<stdio.h>
+int CheckPin(pin)
+{
+    int x;
+    printf("\nEnter your ATM PIN:- ");
+    scanf("%d",&x);
+    if(x==pin)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int main()
 {
     int bal,pin;
     printf("Enter your account balance:- ");
     scanf("&d",&bal);
-    printf("Set four digit pin for ATM:- ");
+    printf("Set four digit PIN for ATM:- ");
     scanf("%d",&pin);
-    char ch=' ';
-    while((ch!='N')&&(ch!='n'))
+    int choice=0;
+    while(choice!=4)
     {
-        int choice;
+        int x=0;
         printf("Select an option from the below menu:-\n");
         printf("1. Check Balance.\n");
         printf("2. Withdraw Money.\n");
@@ -19,6 +33,91 @@ int main()
         printf("Enter your choice (1,2,3,4):- ");
         scanf("%d",&choice);
         if(choice==1)
+        {
+            if(CheckPin)
+            {
+                printf("\nThe current balance in your account is:- %d",bal);
+            }
+            else
+            {
+                printf("\nWrong PIN is entered.");
+                if(CheckPin)
+                {
+                    printf("\nThe current balance in your account is:- %d",bal);
+                }
+                else
+                {
+                    printf("\nWrong PIN is entered.");             
+                    if(CheckPin)
+                    {              
+                        printf("\nThe current balance in your account is:- %d",bal);           
+                    }          
+                    else          
+                    {               
+                        printf("\nWrong PIN is entered.");                
+                        printf("\nYour account is locked.");
+                        break;        
+                    }
+                }
+            }
+        }
+        else if(choice==2)
+        {
+            int amt;
+            printf("\nEnter amount to be withdrawn:- ");
+            scanf("%d",&amt);
+            if(CheckPin)
+            {
+                if(amt<=bal)
+                {
+                    bal=bal-amt;
+                    printf("\nWithdrawal of money is successful.");
+                }
+                else
+                {
+                    printf("\nInsufficient Funds.");
+                }
+            }
+            else
+            {
+                printf("\nWrong PIN is entered.");
+                if(CheckPin)
+                {
+                    if(amt<=bal)
+                    {
+                        bal=bal-amt;
+                        printf("\nWithdrawal of money is successful.");
+                    }
+                    else
+                    {
+                        printf("\nInsufficient Funds.");
+                    }
+                }
+                else
+                {
+                    printf("\nWrong PIN is entered.");             
+                    if(CheckPin)
+                    {              
+                        if(amt<=bal)
+                        {
+                            bal=bal-amt;
+                            printf("\nWithdrawal of money is successful.");
+                        }
+                        else
+                        {
+                            printf("\nInsufficient Funds.");
+                        }      
+                    }          
+                    else          
+                    {               
+                        printf("\nWrong PIN is entered.");                
+                        printf("\nYour account is locked.");
+                        break;        
+                    }
+                }
+            }
+        }
+        elif(choice==3)
         {
             
         }
