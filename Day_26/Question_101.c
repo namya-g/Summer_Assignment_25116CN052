c3ebcbf66aa34d97f9cf7960b6682c88a0458668
namyagupta15-design <namyagupta15@gmail.com>
2026-06-26 20:24:04 +0530

    git init git add Day_26 git commit -m “Day_26 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

---
+++ Day_26/Question_101.c
@@ -0,0 +1,40 @@
+//Q101 Write a program to Create number guessing game.
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    srand(time(NULL));
+    char ch=' ';
+    while((ch!='N')&&(ch!='n'))
+    {
+        int num=(rand()%(99-10+1))+10;
+        printf("Guess the two digit number game:-\n");
+        printf("There will be five guesses.\n");
+        for(int i=1;i<=5;i++)
+        {
+            int n;
+            printf("Guess %d:- ",i);
+            scanf("%d",&n);
+            if(n==num)
+            {
+                printf("You guessed the correct number.\n");
+                break;
+            }
+            else
+            {
+                if(i!=5)
+                {
+                    printf("Try again.\n");
+                }
+                else
+                {
+                    printf("You did not guess the correct number.\n");
+                    printf("The correct number is %d.\n",num);
+                }
+            }
+        }
+        printf("Do you want to play again (y/n):- ");
+        scanf(" %c",&ch);
+    }
+}
