7f8a075163f14ae32e185a8d3fb79b773521fcb4
namyagupta15-design <namyagupta15@gmail.com>
2026-06-27 17:39:45 +0530

    git init git add Day_27 git commit -m “Day_27 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_27/Question_108.c
+++ Day_27/Question_108.c
@@ -83,15 +83,15 @@ int main()
                 printf("\nStudent Name:- %s",s[n].name);
                 printf("\nRoll Number:- %d",s[n].rollno);
                 printf("\n\n**********");
-                printf("\nMarks scored in Subject 1:- %f",s[count].sub1);
-                printf("\nMarks scored in Subject 2:- %f",s[count].sub2);
-                printf("\nMarks scored in Subject 3:- %f",s[count].sub3);
+                printf("\nMarks scored in Subject 1:- %f",s[n].sub1);
+                printf("\nMarks scored in Subject 2:- %f",s[n].sub2);
+                printf("\nMarks scored in Subject 3:- %f",s[n].sub3);
                 printf("\n\n**********");
-                printf("\nTotal marks scored out of 300:- %f",s[count].total);
+                printf("\nTotal marks scored out of 300:- %f",s[n].total);
                 printf("\n\n**********");
-                printf("\nPercentage:- %f",s[count].percentage);
+                printf("\nPercentage:- %f",s[n].percentage);
                 printf("\n\n**********");
-                printf("\nGrade:- %c",s[count].grade);
+                printf("\nGrade:- %c",s[n].grade);
                 printf("\n\n****************************************");
             }
             else
@@ -113,4 +113,108 @@ int main()
                     break;
                 }
             }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nStudent record is found.");
+                printf("\nRecords that can be changed are:-");
+                printf("\n1. Name");
+                printf("\n2. Subject 1 marks");
+                printf("\n3. Subject 2 marks");
+                printf("\n4. Subject 3 marks");
+                printf("\nEnter the record you want to change (1,2,3,4):- ");
+                scanf("%d",&rec);
+                if(rec==1)
+                {
+                    printf("\nEnter new name of the student:- ");
+                    scanf("%s",s[n].name);
+                }
+                else if(rec==2)
+                {
+                    printf("\nEnter new marks of subject 1 of the student:- ");
+                    scanf("%f",&s[n].sub1);
+                }
+                else if(rec==3)
+                {
+                    printf("\nEnter new marks of subject 2 of the student:- ");
+                    scanf("%f",&s[n].sub2);
+                }
+                else if(rec==4)
+                {
+                    printf("\nEnter new marks of subject 3 of the student:- ");
+                    scanf("%f",&s[n].sub3);
+                }
+                else
+                {
+                    printf("\nInvalid choice is entered.");
+                }
+                if((rec==2)||(rec==3)||(rec==4))
+                {
+                    s[n].total=s[n].sub1+s[n].sub2+s[n].sub3;
+                    s[n].percentage=s[n].total/3.0;
+                    if(s[n].percentage>=90)
+                    {                
+                        s[n].grade='A';            
+                    }            
+                    else if(s[n].percentage>=80)            
+                    {                
+                        s[n].grade='B';
             
+                    }            
+                    else if(s[n].percentage>=70)            
+                    {                
+                        s[n].grade='C';            
+                    }            
+                    else if(s[n].percentage>=60)            
+                    {                
+                        s[n].grade='D';            
+                    }            
+                    else            
+                    {                
+                        s[n].grade='F';            
+                    }
+                }
+                printf("\nStudent marksheet record is successfully updated.");
+            }
+            else
+            {
+                printf("\nStudent marksheet record is not found.");
+            }
+        }
+        else if(choice==4)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the roll number of the student whose marksheet record you want to delete:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].rollno==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    for(int j=i;j<count-1;j++)
+                    {
+                        s[j]=s[j+1];
+                    }
+                    count-=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nStudent marksheet record is found and deleted successfully.");
+            }
+            else
+            {
+                printf("\nStudent marksheet record is not found.");
+            }
+        }
+        else
+        {
+            printf("\nInvalid choice is entered.");
+        }
+        printf("\nDo you have other student marksheet inquiries (y/n):- ");
+        scanf(" %c",&ch);
+    }
+}
