cd1e8285bfe54d233f83c0c68cbd6cdbf5cfa05d
namya-g <namyagupta15@gmail.com>
2026-06-28 21:59:12 +0530

    git init git add Day_28 git commit -m “Day_28 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_28/Question_110.c
+++ Day_28/Question_110.c
@@ -1,49 +1,49 @@
-//Q109 Write a program to Create library management system.
+//Q110 Write a program to Create bank account system.
 #include<stdio.h>
-struct Library
+struct BankAccount
 {
         char name[50];
-        int bookid;
-        char author[50];
+        int acc_no;
+        double balance;
         int status;
 };
 int main()
 {
-    struct Library s[100];
-    printf("LIBRARY RECORD MANAGEMENT SYSTEM\n\n");
+    struct BankAccount s[100];
+    printf("BANK ACCOUNT RECORD MANAGEMENT SYSTEM\n\n");
     char ch=' ';
     int count=0;
     while((ch!='N')&&(ch!='n'))
     {
         int choice=0;
         printf("Select an option from the below menu:-\n");
-        printf("1. Add a new book record.\n");
-        printf("2. View a book record.\n");
-        printf("3. Update a book record.\n");
-        printf("4. Delete a book record.\n");
+        printf("1. Add a new bank account record.\n");
+        printf("2. View a bank account record.\n");
+        printf("3. Update a bank account record.\n");
+        printf("4. Delete a bank account record.\n");
         printf("Enter your choice (1,2,3,4):- ");
         scanf("%d",&choice);
         if(choice==1)
         {
-            printf("\nEnter name of the book:- ");
+            printf("\nEnter name onn the bank account:- ");
             scanf("%s",s[count].name);
-            printf("\nEnter book ID of the book:- ");
-            scanf("%d",&s[count].bookid);
-            printf("\nEnter author of the book:- ");
-            scanf("%s",s[count].author);
-            printf("\nEnter status of the book (1. Available 2. Issued):- ");
+            printf("\nEnter account number of the bank account:- ");
+            scanf("%d",&s[count].acc_no);
+            printf("\nEnter balance of the bank account:- ");
+            scanf("%lf",&s[count].balance);
+            printf("\nEnter status of the account (1. Active 2. Inactive):- ");
             scanf("%d",&s[count].status);
             count+=1;
-            printf("\nNew book record is added successfully.");
+            printf("\nNew bank account record is added successfully.");
         }
         else if(choice==2)
         {
             int rn,flag=0,n;
-            printf("\nEnter the book ID of the book whose record you want to search:- ");
+            printf("\nEnter the account number of the bank account whose record you want to search:- ");
             scanf("%d",&rn);
             for(int i=0;i<count;i++)
             {
-                if(s[i].bookid==rn)
+                if(s[i].acc_no==rn)
                 {
                     n=i;
                     flag+=1;
@@ -52,11 +52,11 @@ int main()
             }
             if(flag==1)
             {
-                printf("\nBook record is found.");
-                printf("\nName of the book:- %s",s[n].name);
-                printf("\nBook ID of the book:- %d",s[n].bookid);
-                printf("\nAuthor of the book:- %s",s[n].author);
-                printf("\nStatus of the book:- %d",s[n].status);
+                printf("\nBank account record is found.");
+                printf("\nName on the bank account:- %s",s[n].name);
+                printf("\nAccount number of the bank account:- %d",s[n].acc_no);
+                printf("\nBalance of the bank account:- %lf",&s[n].balance);
+                printf("\nStatus of the bank account:- %d",s[n].status);
             }
             else
             {
