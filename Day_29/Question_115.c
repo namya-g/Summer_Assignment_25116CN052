814d542b370a2f667b51f9ea1ae28b4fa1997087
namya-g <namyagupta15@gmail.com>
2026-06-29 22:45:13 +0530

    git init git add Day_29 git commit -m “Day_29 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_29/Question_115.c
+++ Day_29/Question_115.c
@@ -69,4 +69,117 @@ int main()
                 printf("\nString is not found.\n");
             }
         }
-        
+        else if(choice==3)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the string number of the string you want to update:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].strno==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nString is found.");
+                printf("\nChanges that can be made are:-");
+                printf("\n1. Change a character.");
+                printf("\n2. Delete a character.");
+                printf("\n3. Add a character to the end of the string.");
+                printf("\nEnter the record you want to change (1,2,3):- ");
+                scanf("%d",&rec);
+                if(rec==1)
+                {
+                    int index;
+                    printf("\nEnter index of the character to be changed:- ");
+                    scanf("%d",&index);
+                    if((index>=0)&&(index<s[n].length))
+                    {
+                        printf("Enter new character:- ");
+                        scanf(" %c",&s[n].str[index]);
+                        printf("\nString is updated successfully.\n");
+                    }
+                    else
+                    {
+                        printf("\nInvalid index.\n");
+                    }
+                }
+                else if(rec==2)
+                {
+                    int index;
+                    printf("\nEnter index of the character to be deleted:- ");
+                    scanf("%d",&index);
+                    if((index>=0)&&(index<s[n].length))
+                    {
+                        s[n].length-=1;
+                        for(int i=index;i<=s[n].length;i++) 
+                        {
+                            s[n].str[i]=s[n].str[i+1];
+                        }
+                        printf("\nString is updated successfully.\n");
+                    }
+                    else
+                    {
+                        printf("\nInvalid index is entered.\n");
+                    }
+                }
+                else if(rec==3)
+                {
+                    printf("\nEnter character to be added to the string:- ");
+                    scanf(" %c",&s[n].str[s[n].length]);
+                    s[n].length+=1;
+                    s[n].str[s[n].length]='\0'; 
+                    printf("\nString is updated successfully.\n");
+                }
+                else
+                {
+                    printf("\nInvalid choice is entered.\n");
+                }
+            }
+            else
+            {
+                printf("\nString is not found.\n");
+            }
+        }
+        else if(choice==4)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the string number of the string you want to delete:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].strno==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    for(int j=i;j<count-1;j++)
+                    {
+                        s[j]=s[j+1];
+                    }
+                    count-=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                printf("\nString is found and deleted successfully.\n");
+            }
+            else
+            {
+                printf("\nString is not found.\n");
+            }
+        }
+        else
+        {
+            printf("\nInvalid choice is entered.\n");
+        }
+        printf("\nDo you have other string inquiries (y/n):- ");
+        scanf(" %c",&ch);
+    }
+    return 0;
+}
