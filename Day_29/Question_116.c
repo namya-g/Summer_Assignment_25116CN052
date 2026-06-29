45ca84ea30b5ec4ad1965741a7ab30ece290d921
namya-g <namyagupta15@gmail.com>
2026-06-29 22:58:01 +0530

    git init git add Day_29 git commit -m “Day_29 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_29/Question_116.c
+++ Day_29/Question_116.c
@@ -68,4 +68,85 @@ int main()
                 printf("\nItem is not found.\n");
             }
         }
-        
+        else if(choice==3)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the item number of the item you want to update:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].itemno==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nItem is found.");
+                printf("\nChanges that can be made are:-");
+                printf("\n1. Update quantity (Restock/Sell).");
+                printf("\n2. Update price.");
+                printf("\nEnter the record you want to change (1,2):- ");
+                scanf("%d",&rec);
+                if(rec==1)
+                {
+                    printf("\nEnter new quantity of the item:- ");
+                    scanf("%d",&s[n].quantity);
+                    printf("\nItem is updated successfully.\n");
+                }
+                else if(rec==2)
+                {
+                    printf("\nEnter new price of the item:- ");
+                    scanf("%f",&s[n].price);
+                    printf("\nItem is updated successfully.\n");
+                }
+                else
+                {
+                    printf("\nInvalid choice is entered.\n");
+                }
+            }
+            else
+            {
+                printf("\nItem is not found.\n");
+            }
+        }
+        else if(choice==4)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the item number of the item you want to delete:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].itemno==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    for(int j=i;j<count-1;j++)
+                    {
+                        s[j]=s[j+1];
+                    }
+                    count-=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                printf("\nItem is found and deleted successfully.\n");
+            }
+            else
+            {
+                printf("\nItem is not found.\n");
+            }
+        }
+        else
+        {
+            printf("\nInvalid choice is entered.\n");
+        }
+        printf("\nDo you have other inventory inquiries (y/n):- ");
+        scanf(" %c",&ch);
+    }
+    return 0;
+}
