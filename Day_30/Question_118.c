f086eacb28e1a499326ad233aac376056bd3bb45
namya-g <namyagupta15@gmail.com>
2026-06-30 15:40:28 +0530

    git init git add Day_30 git commit -m “Day_30 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_30/Question_118.c
+++ Day_30/Question_118.c
@@ -63,4 +63,93 @@ int main()
                 printf("\nBook record is not found.");
             }
         }
-        
+        else if(choice==3)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the book ID of the book whose record you want to update:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].bookid==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nBook record is found.");
+                printf("\nRecords that can be changed are:-");
+                printf("\n1. Name");
+                printf("\n2. Author");
+                printf("\n3. Status");
+                printf("\nEnter the record you want to change (1,2,3):- ");
+                scanf("%d",&rec);
+                if(rec==1)
+                {
+                    printf("\nEnter new name of the book:- ");
+                    scanf("%s",s[n].name);
+                    printf("\nBook record is updated successfully.");
+                }
+                else if(rec==2)
+                {
+                    printf("\nEnter new author of the book:- ");
+                    scanf("%s",s[n].author);
+                    printf("\nBook record is updated successfully.");
+                }
+                else if(rec==3)
+                {
+                    printf("\nEnter new status of the book:- ");
+                    scanf("%d",&s[n].status);
+                    printf("\nBook record is updated successfully.");
+                }
+                else
+                {
+                    printf("\nInvalid choice is entered.");
+                }
+            }
+            else
+            {
+                printf("\nBook record is not found.");
+            }
+        }
+        else if(choice==4)
+        {
+            int rn,flag=0,n;
+            printf("\nEnter the book ID of the book whose record you want to delete:- ");
+            scanf("%d",&rn);
+            for(int i=0;i<count;i++)
+            {
+                if(s[i].bookid==rn)
+                {
+                    n=i;
+                    flag+=1;
+                    for(int j=i;j<count-1;j++)
+                    {
+                        s[j]=s[j+1];
+                    }
+                    count-=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            {
+                int rec=0;
+                printf("\nBook record is found and deleted successfully.");
+            }
+            else
+            {
+                printf("\nBook record is not found.");
+            }
+        }
+        else
+        {
+            printf("\nInvalid choice is entered.");
+        }
+        printf("\nDo you have other book inquiries (y/n):- ");
+        scanf(" %c",&ch);
+    }
+    return 0;
+}
