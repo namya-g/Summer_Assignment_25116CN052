17f8701be6cf7b1a8883212f8fb625b332ca5b36
namya-g <namyagupta15@gmail.com>
2026-06-30 16:13:15 +0530

    git init git add Day_30 git commit -m “Day_30 uploaded.” git branch -M main git remote add origin  https://github.com/namya-g/Summer_Assignment_25116CN052 git push -u origin main

--- Day_30/Question_120.c
+++ Day_30/Question_120.c
@@ -59,4 +59,77 @@ void generateBill(struct Item m[],int count)
         printf("\nPlease add items to the menu first before taking an order.");
         return;
     }
-    
+    int order_id,qty;
+    float total=0.0;
+    char add_more='y';
+    printf("\n--- NEW CUSTOMER ORDER ---");
+    while((add_more == 'y')||(add_more == 'Y'))
+    {
+        int flag=0;
+        printf("\nEnter Item ID to order:- ");
+        scanf("%d",&order_id);
+        for(int i=0;i<count;i++)
+        {
+            if(m[i].id==order_id)
+            {
+                printf("Enter quantity for %s:- ",m[i].name);
+                scanf("%d",&qty);
+                float cost=m[i].price*qty;
+                total+=cost;
+                printf("Added %d x %s = %.2f\n",qty,m[i].name,cost);
+                flag+=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            printf("\nInvalid Item ID entered.");
+        }
+        printf("\nAdd another item to this customer's order? (y/n):- ");
+        scanf(" %c",&add_more);
+    }
+    printf("\n--------------------------------");
+    printf("\nTOTAL BILL AMOUNT: %.2f", total);
+    printf("\n--------------------------------");
+}
+int main()
+{
+    struct Item menu[100];
+    printf("CAFE ORDER AND BILLING SYSTEM\n\n"); 
+    char ch=' ';
+    int count=0;
+    while((ch != 'N')&&(ch != 'n'))
+    {
+        int choice=0;
+        printf("\nSelect an option from the below menu:-\n");
+        printf("1. Add a new menu item.\n");
+        printf("2. View the cafe menu.\n");
+        printf("3. Update an item's price.\n");
+        printf("4. Take an order & generate bill.\n");
+        printf("Enter your choice (1,2,3,4):- ");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            count=addMenuItem(menu,count); 
+        }
+        else if(choice==2)
+        {
+            viewMenu(menu,count);
+        }
+        else if(choice==3)
+        {
+            updatePrice(menu,count);
+        }
+        else if(choice==4)
+        {
+            generateBill(menu,count);
+        }
+        else
+        {
+            printf("\nInvalid choice is entered.");
+        }
+        printf("\n\nDo you have other cafe operations (y/n):- ");
+        scanf(" %c",&ch);
+    }
+    return 0;
+}
